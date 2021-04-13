7cabdd8d270cc9ffaaa10c99e72d71933deb41e2
Ken Kawamoto <ken@kawamoto.co.uk>
2021-04-13 12:08:02 +0900

    use redirect instead, as rewrite somehow didn't work.

--- web/next.config.js
+++ web/next.config.js
@@ -1,9 +1,18 @@
 module.exports = {
-    async rewrites() {
+    // async rewrites() {
+    //     return [
+    //         {
+    //             source: '/',
+    //             destination: '/map/35.699728912010656,139.72915375605228/35.6952720844853,139.73539701662958',
+    //         },
+    //     ]
+    // },
+    async redirects() {
         return [
             {
                 source: '/',
                 destination: '/map/35.699728912010656,139.72915375605228/35.6952720844853,139.73539701662958',
+                permanent: true,
             },
         ]
     },
