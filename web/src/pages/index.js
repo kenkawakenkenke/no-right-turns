2b9dfbcb86d667303c63c0d7bd4228d731a61052
Ken Kawamoto <ken@kawamoto.co.uk>
2021-04-13 12:03:46 +0900

    Setup root path rewrite

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -6,11 +6,11 @@ export default function Home() {
   return <div>This shouldn't show</div>
 }
 
-export async function getStaticProps(context) {
-  return {
-    redirect: {
-      destination: '/map/35.699728912010656,139.72915375605228/35.6952720844853,139.73539701662958',
-      permanent: true,
-    },
-  }
-}
+// export async function getStaticProps(context) {
+//   return {
+//     redirect: {
+//       destination: '/map/35.699728912010656,139.72915375605228/35.6952720844853,139.73539701662958',
+//       permanent: true,
+//     },
+//   }
+// }
